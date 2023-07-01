678334c1b75930bb73ea2a0170489b9c0641b7b7
Kanisk <57884127+chakrakan@users.noreply.github.com>
2023-07-01 14:51:33 -0400

    Update flatten.rb

--- flatten.rb
+++ flatten.rb
@@ -1,10 +1,10 @@
-X86_64_SHA = "ab38985cadb4a4af28335d1a68ca92aa40c942b2337e1399b46bcd7a6b546055"
-VERSION = "1.0"
+X86_64_SHA = "2e2056c71a0264e4d1592b098629ebde2940b67bace6501e17d23f6b8b5f5e2e"
+VERSION = "0.1.0"
 
 class Flatten < Formula
   desc "A command-line tool to flatten directory structures"
   homepage "https://github.com/chakrakan/flatten"
-  url "https://github.com/chakrakan/flatten/releases/download/v#{VERSION}/flatten"
+  url "https://github.com/chakrakan/flatten/releases/download/v#{VERSION}/flatten-v#{VERSION}-mac.tar.gz"
   sha256 X86_64_SHA
 
   def install
